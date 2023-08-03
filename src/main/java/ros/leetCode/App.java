99709d50df8aca08563d9b8db34e2c81ac0a54a6
Ros <traine41@icloud.com>
2023-08-03 17:23:38 +0100

    test commit

---
+++ src/main/java/ros/leetCode/App.java
@@ -0,0 +1,13 @@
+package ros.leetCode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
