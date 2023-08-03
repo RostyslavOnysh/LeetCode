67bc9c63ced185d7b25fa21135655c50d9c22a87
Ros <traine41@icloud.com>
2023-08-03 18:39:18 +0100

    #3 Update

--- src/main/java/LeetCode/twoSum/SecondVariant.java
+++ src/main/java/LeetCode/twoSum/SecondVariant.java
@@ -2,22 +2,21 @@ package LeetCode.twoSum;
 
 import java.util.HashMap;
 
-public class Solution {
+public class SecondVariant {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> numIndices = new HashMap<>();
+
         int index = 0;
         for (int num : nums) {
+            int complement = target - num;
+            int complementIndex = numIndices.containsKey(complement) ? numIndices.get(complement) : -1;
+            if (complementIndex != -1 && complementIndex != index) {
+                return new int[]{complementIndex, index};
+            }
             numIndices.put(num, index);
             index++;
         }
-        index = 0;
-        for (int num : nums) {
-            int complement = target - num;
-            if (numIndices.containsKey(complement) && numIndices.get(complement) != index) {
-                return new int[]{index, numIndices.get(complement)};
-            }
-            index++;
-        }
+
         return null;
     }
 }
