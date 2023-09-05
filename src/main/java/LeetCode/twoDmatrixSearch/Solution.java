4d87fc87315299f3f298ca4d4358b114336e11b9
Ros <traine41@icloud.com>
2023-09-05 21:37:46 +0100

    980 M F I
    LeetCode For Interview

---
+++ src/main/java/LeetCode/twoDmatrixSearch/Solution.java
@@ -0,0 +1,46 @@
+package LeetCode.twoDmatrixSearch;
+
+import static performanceCheker.Performance.logPerformance;
+
+public class Solution {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+
+        while (i < matrix.length && j >= 0) {
+
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] < target) {
+                i++;
+            } else j--;
+        }
+        return false;
+    }
+}
+
+ /*   public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11},
+                {2, 5, 8, 12},
+                {3, 6, 9, 16},
+                {10, 13, 14, 17}
+        };
+
+        int target1 = 5;
+        int target2 = 20;
+        logPerformance();
+        // Call the searchMatrix method to search for target1
+        boolean found1 = searchMatrix(matrix, target1);
+        System.out.println("Target 1 found: " + found1); // Should print "Target 1 found: true"
+
+        // Call the searchMatrix method to search for target2
+        boolean found2 = searchMatrix(matrix, target2);
+        System.out.println("Target 2 found: " + found2); // Should print "Target 2 found: false"
+        logPerformance();
+    }
+}
+
+
+  */
