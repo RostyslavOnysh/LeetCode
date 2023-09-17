b0c6a3efc9d75f75d11a05b74e6330d6f8b5153b
Ros <traine41@icloud.com>
2023-09-17 18:17:17 +0100

    980 M F I
    LeetCode For Interview

--- src/main/java/LeetCode/RomanToInt/Solution.java
+++ src/main/java/LeetCode/RomanToInt/Solution.java
@@ -1,9 +1,32 @@
 package LeetCode.RomanToInt;
 
-public class Solution {
-    public int romanToInt(String s) {
-        short[] romanValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        return  0;
-    }
-    }
+import java.util.HashMap;
 
+public class Solution {
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> values = new HashMap<>();
+        values.put('I', 1);
+        values.put('V', 5);
+        values.put('X', 10);
+        values.put('L', 50);
+        values.put('C', 100);
+        values.put('D', 500);
+        values.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            int currentValue = values.get(currentChar);
+
+            if (currentValue > prevValue) {
+                result += (currentValue - 2 * prevValue);
+            } else {
+                result += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return result;
+    }
+}
