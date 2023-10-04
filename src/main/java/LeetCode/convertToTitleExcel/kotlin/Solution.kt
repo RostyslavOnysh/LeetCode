126c2540621c87a8c18eb150111c86db6f57882c
Ros <traine41@icloud.com>
2023-10-04 18:38:39 +0100

    LeetCode (Kotlin) prep interview 9667

---
+++ src/main/java/LeetCode/convertToTitleExcel/kotlin/Solution.kt
@@ -0,0 +1,21 @@
+package LeetCode.convertToTitleExcel.kotlin
+
+fun main() {
+    val solution = Solution()
+    val columnNumber = 2
+    val columnTitle = solution.convertToTitle(columnNumber)
+    println("Column title for $columnNumber is $columnTitle")
+}
+class Solution {
+    fun convertToTitle(columnNumber: Int): String {
+        val result = StringBuilder()
+        var num = columnNumber
+        while (num > 0) {
+            val remainder = (num - 1) % 26
+            val letter = 'A' + remainder
+            result.insert(0, letter)
+            num = (num - 1) / 26
+        }
+        return result.toString()
+    }
+}
