1487430dc38faebeccf4d16a7c2991d5d80d2e74
Ros <traine41@icloud.com>
2023-10-04 19:02:42 +0100

    LeetCode (Kotlin) prep interview 9667

---
+++ src/main/java/LeetCode/ExcelSheetColumnNumber/Solution.java
@@ -0,0 +1,13 @@
+package LeetCode.ExcelSheetColumnNumber;
+
+public class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (int i = 0; i < columnTitle.length(); i++) {
+            char c = columnTitle.charAt(i);
+            int charValue = c - 'A' + 1;
+            result = result * 26 + charValue;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/LeetCode/ExcelSheetColumnNumber/kotlin/Solution.kt
@@ -0,0 +1,20 @@
+package LeetCode.ExcelSheetColumnNumber.kotlin
+
+
+fun main() {
+    val solution = Solution()
+    val columnTitle = "CA"
+    val columnNumber = solution.titleToNumber(columnTitle)
+    println("Column title for $columnTitle is $columnNumber")
+}
+
+class Solution {
+    fun titleToNumber(columnTitle: String): Int {
+        var result = 0
+        for (i in 0 until columnTitle.length) {
+            val charValue = columnTitle[i] - 'A' + 1
+            result = result * 26 + charValue
+        }
+        return result
+    }
+}
