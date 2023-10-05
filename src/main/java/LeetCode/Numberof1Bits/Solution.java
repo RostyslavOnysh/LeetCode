3ab3509b59a3c707b27848bbf50070cadfe52092
Ros <traine41@icloud.com>
2023-10-05 12:35:09 +0100

    LeetCode (Kotlin) prep interview 9667

---
+++ src/main/java/LeetCode/Numberof1Bits/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.Numberof1Bits;
+
+public class Solution {
+    public int hammingWeight(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 1) {
+                result++;
+            }
+            n >>= 1;
+        }
+        return result;
+    }
+}
