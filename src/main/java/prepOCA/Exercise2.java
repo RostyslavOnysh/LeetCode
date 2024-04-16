8c3f6e235725383d953475bab7705d20771d05ad
Ros <traine41@icloud.com>
2024-04-16 14:28:08 +0100

    OCA java prep

--- src/main/java/prepOCA/Exercise2.java
+++ src/main/java/prepOCA/Exercise2.java
@@ -2,33 +2,36 @@ package prepOCA;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        double availableAmount = 500.0;
-        double[] loanRequests = {60.0, 20.0, 100.0, 80.0, 40.0, 300.0, 200.0, 100.0};
+        int cash = 500;
 
-        for (double loanRequest : loanRequests) {
-            System.out.println("Cash in the pot: €" + availableAmount);
-            System.out.println("Loan amount requested: €" + loanRequest);
+        // Loan request amounts
+        int[] loanRequests = {60, 20, 100, 80, 40, 300, 200, 100};
 
-            if (loanRequest <= availableAmount) {
-                System.out.println("  Loan amount granted!");
-                availableAmount -= loanRequest;
-                System.out.println("  Cash in the pot: €" + availableAmount);
-            } else if (availableAmount > 0.0) {
-                System.out.println("  The exact loan request amount cannot be processed in full (insufficient funds available).");
-                double partialLoan = availableAmount;
-                availableAmount = 0.0;
-                System.out.println("  However, we will give you what we can... €" + partialLoan);
-                System.out.println("  Cash remaining in the pot: €" + availableAmount);
+        // Process each loan request
+        for (int loan : loanRequests) {
+            System.out.println("Cash in the pot: " + cash);
+            System.out.print("Loan amount requested: " + loan + " - ");
+
+            // Check if there's enough cash
+            if (cash >= loan) {
+                cash -= loan;
+                System.out.println("Loan amount granted!");
             } else {
-                System.out.println("  Insufficient funds available. Loan request rejected.");
+                System.out.println("The exact loan request amount cannot be processed in full (insufficient funds available).");
+                System.out.println("However, we will give you what we can..." + cash);
+                cash = 0; // Set cash to 0 as all remaining is given
             }
+
+            System.out.println("Cash remaining in the pot: " + cash);
             System.out.println();
         }
-        if (availableAmount == 0.0) {
+
+        // Display outstanding loan requests (if any)
+        if (cash == 0) {
             System.out.println("The following loan requests could not be facilitated:");
-            for (double loanRequest : loanRequests) {
-                if (loanRequest > 500.0 - loanRequest) {
-                    System.out.println("- €" + loanRequest);
+            for (int loan : loanRequests) {
+                if (loan > cash) {
+                    System.out.println(loan);
                 }
             }
         }
