a9ddd01bfdacae0b50b2710bf11d5ecef0da8e01
Ros <traine41@icloud.com>
2024-04-17 14:32:48 +0100

    OCA java prep

--- src/main/java/prepOCA/Exercise2.java
+++ src/main/java/prepOCA/Exercise2.java
@@ -2,37 +2,39 @@ package prepOCA;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        double availableAmount = 500.0;
-        double[] loanRequests = {60.0, 20.0, 100.0, 80.0, 40.0, 300.0, 200.0, 100.0};
-        int unfacilitatedCount = 0;
-        boolean partialLoanProcessed = false;
+        int availableAmount = 500;
+        int[] loanRequests = {60, 20, 100, 80, 40, 300, 200, 100};
+        boolean[] loanFulfilled = new boolean[loanRequests.length];
+        boolean allLoansFulfilled = true;
 
-        for (double loanRequest : loanRequests) {
-            System.out.println("Cash in the pot: " + (int) availableAmount);
+        for (int i = 0; i < loanRequests.length; i++) {
+            double loanRequest = loanRequests[i];
+            System.out.println("Cash in the pot: " + availableAmount);
             System.out.print("Loan amount requested: " + (int) loanRequest);
-
             if (loanRequest <= availableAmount) {
                 availableAmount -= loanRequest;
                 System.out.println(" - Loan amount granted!");
+                loanFulfilled[i] = true;
             } else {
-                if (!partialLoanProcessed) {
-                    System.out.println(" - The exact loan request amount cannot be processed in full (insufficient funds available).");
-                    double partialLoan = availableAmount;
-                    availableAmount = 0.0;
-                    System.out.println("However, we will give you what we can... " + (int) partialLoan);
-                    System.out.println("\nCash remaining in the pot: " + (int) availableAmount);
-                    unfacilitatedCount++;
-                }
-                unfacilitatedCount++;
+                System.out.println("\nThe exact loan request amount cannot be processed in full (insufficient funds available).");
+                double partialLoan = availableAmount;
+                availableAmount = 0;
+                System.out.println("However, we will give you what we can... " + partialLoan);
+                System.out.println("\nCash remaining in the pot: " + availableAmount);
+                loanFulfilled[i] = true;
+                allLoansFulfilled = false;
                 break;
             }
             System.out.println();
         }
 
-        System.out.println("\nThe following loan requests could not be facilitated:");
-        for (int i = 0; i < unfacilitatedCount; i++) {
-            double loanRequest = loanRequests[i + loanRequests.length - unfacilitatedCount];
-            System.out.println("€" + (int) loanRequest);
+        if (!allLoansFulfilled) {
+            System.out.println("The following loan requests could not be facilitated:");
+            for (int i = 0; i < loanRequests.length; i++) {
+                if (!loanFulfilled[i]) {
+                    System.out.println("€" + loanRequests[i]);
+                }
+            }
         }
     }
 }
