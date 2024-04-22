a24a3cc04dbe6da6ab8eae81786a066d5b775f43
Ros <traine41@icloud.com>
2024-04-22 13:50:26 +0100

    OCA java prep

--- src/main/java/prepOCA/Animal.java
+++ src/main/java/prepOCA/Animal.java
@@ -1,14 +1,30 @@
 package prepOCA;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 class Animal {
-    public static void main(String[] args) {
-        System.out.printf("The value of PI is: %.2f", Math.PI);
+    public static void main(String[] args){
+        new Animal().writeToFile();
+    }
+
+    public void writeToFile(){
+        File file = new File("Write.txt");
+
+        try{
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println("Hello World!");
+            pw.close();
+        }catch(IOException e){
+            System.out.println("Unable to write to the file at this time.");
+        }
     }
 }
