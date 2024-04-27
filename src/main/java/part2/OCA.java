3791b227afefe4dc9db0c09ec252e5544acd1b83
Ros <traine41@icloud.com>
2024-04-27 18:45:51 +0100

    OCA java prep

---
+++ src/main/java/part2/OCA.java
@@ -0,0 +1,16 @@
+package part2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OCA {
+    public static void main(String[] args) {
+        int[] array = {6, 9, 8};
+        List<Integer> list = new ArrayList<>();
+        list.add(array[0]);
+        list.add(array[2]);
+        list.set(1, array[1]);
+        list.remove(0);
+        System.out.println(list);
+    }
+}
