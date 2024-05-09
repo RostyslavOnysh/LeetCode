5299c75c9cdeb97c9dc4e18cd6dbdede9784cc01
Ros <traine41@icloud.com>
2024-05-09 12:46:13 +0100

    OCA java prep

--- src/main/java/LeetCode/task_219_Contains_Duplicate_II/Solution.java
+++ src/main/java/LeetCode/task_219_Contains_Duplicate_II/Solution.java
@@ -6,20 +6,16 @@ import java.util.Map;
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            // Перевіряємо, чи ми вже зустрічали поточне число
             if (map.containsKey(nums[i])) {
-                // Якщо так, перевіряємо різницю між поточним і попереднім індексом
                 int prevIndex = map.get(nums[i]);
                 if (Math.abs(i - prevIndex) <= k) {
-                    return true; // Знайдено такі індекси
+                    return true;
                 }
             }
-            // Зберігаємо або оновлюємо індекс поточного числа
             map.put(nums[i], i);
         }
 
-        return false; // Немає таких індексів
+        return false;
     }
 }
