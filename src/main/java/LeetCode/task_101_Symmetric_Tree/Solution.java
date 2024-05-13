6a0813180323f0bf12cdee6cb151d3acbc99cb5f
Ros <traine41@icloud.com>
2024-05-13 13:36:26 +0100

    OCA java prep

---
+++ src/main/java/LeetCode/task_101_Symmetric_Tree/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.task_101_Symmetric_Tree;
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return (left.val == right.val)
+                && isMirror(left.left, right.right)
+                && isMirror(left.right, right.left);
+    }
+}
---
+++ src/main/java/LeetCode/task_389_FindTheDifference/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.task_389_FindTheDifference;
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        char result = 0;
+        for (char c : s.toCharArray()) {
+            result ^= c;
+        }
+        for (char d : t.toCharArray()) {
+            result ^= d;
+        }
+        return result;
+    }
+}
