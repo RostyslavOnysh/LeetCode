76c6228d5fcbdeabfbd1d475f0c29361038ee49a
Ros <traine41@icloud.com>
2024-07-01 18:50:41 +0100

    OCA java prep

--- src/main/java/hackerrank/warmup/Staircase.java
+++ src/main/java/hackerrank/warmup/Staircase.java
@@ -3,15 +3,16 @@ package hackerrank.warmup;
 public class Staircase {
     public static void staircase(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - 1; j++) {
-                System.out.println(" ");
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
-                System.out.println("#");
+                System.out.print("#");
             }
             System.out.println();
         }
     }
+
 
     public static void main(String[] args) {
         int n = 6;
