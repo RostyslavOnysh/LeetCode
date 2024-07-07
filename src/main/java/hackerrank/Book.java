1d4018e7a8ff5d0fd46ad419f57856a09c8c9373
Ros <traine41@icloud.com>
2024-07-07 12:35:21 +0200

    OCA java prep

---
+++ src/main/java/hackerrank/Book.java
@@ -0,0 +1,15 @@
+package hackerrank;
+
+public  abstract class Book {
+    String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+}
+
+class MyBook extends Book {
+    void setTitle (String s) {
+        this.title = s;
+    }
+}
