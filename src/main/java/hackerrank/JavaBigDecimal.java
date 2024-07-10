1502685344663fb87c0b3da496e0c9108d5bfef6
Ros <traine41@icloud.com>
2024-07-10 19:17:04 +0100

    OCA java prep

---
+++ src/main/java/hackerrank/JavaBigDecimal.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class JavaBigDecimal {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                BigDecimal bd1 = new BigDecimal(s[j]);
+                BigDecimal bd2 = new BigDecimal(s[j + 1]);
+                if (bd1.compareTo(bd2) < 0) {
+                    String temp = s[j];
+                    s[j] = s[j + 1];
+                    s[j + 1] = temp;
+                }
+            }
+        }
+
+
+        //Output
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
