24fd122f05193b5a276d398b6c2df9e491d60bd1
Ros <traine41@icloud.com>
2024-07-18 14:57:57 +0100

    task_2000_Reverse_Prefix_of_Word

--- src/main/java/LeetCode/task_2000_Reverse_Prefix_of_Word/Solution.java
+++ src/main/java/LeetCode/task_2000_Reverse_Prefix_of_Word/Solution.java
@@ -4,29 +4,26 @@ import java.util.Stack;
 
 public class Solution {
     public String reversePrefix(String word, char ch) {
+        StringBuilder sb = new StringBuilder();
         Stack<Character> stack = new Stack<>();
-        StringBuilder result = new StringBuilder();
-
         int index = 0;
+
         while (index < word.length()) {
             stack.push(word.charAt(index));
-            // Found ch
+
             if (word.charAt(index) == ch) {
-                // Add characters through ch to the result in reverse order
                 while (!stack.isEmpty()) {
-                    result.append(stack.pop());
+                    sb.append(stack.pop());
                 }
                 index++;
-                // Add the rest of the characters to result
-                while (index < word.length()) {
-                    result.append(word.charAt(index));
-                    index++;
-                }
-                return result.toString();
             }
-            index++;
+            while (index < word.length()) {
+                sb.append(word.charAt(index));
+                index++;
+            }
+            return sb.toString();
         }
-
+        index++;
         return word;
     }
 }
