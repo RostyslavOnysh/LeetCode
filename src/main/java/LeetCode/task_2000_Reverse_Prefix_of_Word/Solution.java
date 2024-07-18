29e2fc4293bba6bd41da683fec2d52834ede8f67
Ros <traine41@icloud.com>
2024-07-18 15:28:44 +0100

    task_2000_Reverse_Prefix_of_Word

--- src/main/java/LeetCode/task_2000_Reverse_Prefix_of_Word/Solution.java
+++ src/main/java/LeetCode/task_2000_Reverse_Prefix_of_Word/Solution.java
@@ -4,26 +4,48 @@ import java.util.Stack;
 
 public class Solution {
     public String reversePrefix(String word, char ch) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
-        int index = 0;
+        int left = 0;
+        char [] result = word.toCharArray();
 
+        for (int right = 0; right < word.length(); right++) {
+            if (result[right] == ch) {
+                while (left <= right) {
+                    swap(result, left, right);
+                    left++;
+                    right--;
+                }
+                return new String(result);
+            }
+        }
+        return word;
+    }
+
+    private void swap (char [] chars, int index1, int index2) {
+        char temp = chars[index2];
+        chars[index2] = chars[index1];
+        chars[index1] = temp;
+    }
+}
+       /* Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        int index = 0;
         while (index < word.length()) {
             stack.push(word.charAt(index));
-
             if (word.charAt(index) == ch) {
                 while (!stack.isEmpty()) {
-                    sb.append(stack.pop());
+                    result.append(stack.pop());
                 }
                 index++;
+                while (index < word.length()) {
+                    result.append(word.charAt(index));
+                    index++;
+                }
+                return result.toString();
             }
-            while (index < word.length()) {
-                sb.append(word.charAt(index));
-                index++;
-            }
-            return sb.toString();
+            index++;
         }
-        index++;
         return word;
     }
 }
+
+        */
