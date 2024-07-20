6b2e02810c32453bb7b2ff0fa094611e226dca65
Ros <traine41@icloud.com>
2024-07-20 20:12:05 +0100

    new solution

---
+++ src/main/java/LeetCode/task_1773/_Count_Items_Matching_a_Rule/Solution.java
@@ -0,0 +1,19 @@
+package LeetCode.task_1773._Count_Items_Matching_a_Rule;
+
+import java.util.List;
+
+public class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int NumberOfMatchedItems = 0;
+        int j = 0;
+        switch(ruleKey){
+            case "color" -> j = 1;
+            case "name" -> j = 2;
+        }
+        for(List<String> i: items){
+            if(i.get(j).equals(ruleValue))NumberOfMatchedItems++;
+        }
+
+        return NumberOfMatchedItems;
+    }
+}
