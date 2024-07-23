4dda1183d63a49cbd09406400a5e6b421c7807f0
Ros <traine41@icloud.com>
2024-07-23 11:50:13 +0100

    leetcode

--- src/main/java/LeetCode/task_1816/_Truncate_Sentence/Solution2.java
+++ src/main/java/LeetCode/task_1816/_Truncate_Sentence/Solution2.java
@@ -2,13 +2,12 @@ package LeetCode.task_1816._Truncate_Sentence;
 
 public class Solution2 {
     public String truncateSentence(String s, int k) {
-        int i = 0;
-        int count = 0;
         int n = s.length();
+        int count = 0;
+        int i = 0;
         while (i < n) {
             if (s.charAt(i) == ' ') {
                 count++;
-
                 if (count == k) {
                     return s.substring(0,i);
                 }
