17b767a57880d5594bf4cf4f652fc50a8e67d881
Ros <traine41@icloud.com>
2024-07-27 15:55:36 +0100

    task_2810_Faulty_Keyboard;

--- src/main/java/LeetCode/task_2810_Faulty_Keyboard/Solution.java
+++ src/main/java/LeetCode/task_2810_Faulty_Keyboard/Solution.java
@@ -2,7 +2,7 @@ package LeetCode.task_2810_Faulty_Keyboard;
 
 public class Solution {
     public String finalString(String s) {
-        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'i') {
                 sb.reverse();
