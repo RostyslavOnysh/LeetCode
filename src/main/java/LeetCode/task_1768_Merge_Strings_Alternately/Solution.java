60444ece0c3bf2a7e6a0cb6d86cc9b8fdb4ec43f
Ros <traine41@icloud.com>
2024-08-02 19:18:54 +0100

    task_1768_Merge_Strings_Alternately;

---
+++ src/main/java/LeetCode/task_1768_Merge_Strings_Alternately/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode.task_1768_Merge_Strings_Alternately;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0, j = 0;
+        StringBuilder sb = new StringBuilder();
+
+
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i));
+                i++;
+            }
+            if (j < word2.length()) {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return sb.toString();
+    }
+}
