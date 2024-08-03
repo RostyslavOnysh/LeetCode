33f6c8ca0252cd863be1588bec95065a8e7b5d57
Ros <traine41@icloud.com>
2024-08-03 18:51:11 +0100

    task_392_Is_Subsequence;

---
+++ src/main/java/LeetCode/task_392_Is_Subsequence/Solution.java
@@ -0,0 +1,14 @@
+package LeetCode.task_392_Is_Subsequence;
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
