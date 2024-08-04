6a2c6d48ca4abb522641ba288130f8264555b35e
Ros <traine41@icloud.com>
2024-08-04 20:56:32 +0100

    task_4_Longest_Common_Prefix;

--- src/main/java/LeetCode/task_4_Longest_Common_Prefix/Solution.java
+++ src/main/java/LeetCode/task_4_Longest_Common_Prefix/Solution.java
@@ -2,21 +2,19 @@ package LeetCode.task_4_Longest_Common_Prefix;
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-            if(strs.length == 0) {
-                return "";
-            }
-            String result = strs[0];
+        if (strs.length == 0) {
+            return "";
+        }
+        String result = strs[0];
 
-            for (int i = 1; i < strs.length; i++) {
-                while (strs[i].indexOf(result) != 0) {
-                    result = result.substring(0, result.length() - 1);
-                    if (result.isEmpty()) {
-                        return "";
-                    }
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(result) != 0) {
+                result = result.substring(0, result.length() - 1);
+                if (result.isEmpty()) {
+                    return "";
                 }
             }
-            return result;
         }
-    }
+        return result;
     }
 }
