974ce8e36dc3bea264677de3f709b3f9791edb29
Ros <traine41@icloud.com>
2024-08-09 15:54:45 +0100

    task_1859_Sorting_the_Sentence

--- src/main/java/LeetCode/task_1859_Sorting_the_Sentence/Solution.java
+++ src/main/java/LeetCode/task_1859_Sorting_the_Sentence/Solution.java
@@ -2,18 +2,19 @@ package LeetCode.task_1859_Sorting_the_Sentence;
 
 public class Solution {
     public String sortSentence(String s) {
-      String [] str = s.split(" ");
-      String [] res = new String[str.length];
+        String[] str = s.split(" ");
+        String[] res = new String[str.length];
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (String elem : str) {
+            i = (int) (elem.charAt(elem.length() - 1) - '0');
+            res[i - 1] = elem.substring(0, elem.length() - 1);
+        }
+        for (i = 0; i < res.length - 1; i++) {
+            sb.append(res[i]).append(" ");
 
-      for (String word : str) {
-          int pos = word.charAt(word.length() - 1) - '0';
-          res[pos - 1] = word.substring(0, word.length() - 1);
-      }
-
-      StringBuilder sb =new StringBuilder();
-      for (String word : res) {
-          sb.append(word).append(" ");
-      }
-      return sb.toString();
+        }
+        sb.append(res[i]);
+        return sb.toString();
     }
 }
