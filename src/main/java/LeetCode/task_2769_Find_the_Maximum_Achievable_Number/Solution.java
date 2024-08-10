ef716600de8bcbfcfaa1fdfd2f545caeda8ad430
Ros <traine41@icloud.com>
2024-08-10 18:31:33 +0100

    LeetCode.task_2469_Convert_the_Temperature;

--- src/main/java/LeetCode/task_2769_Find_the_Maximum_Achievable_Number/Solution.java
+++ src/main/java/LeetCode/task_2769_Find_the_Maximum_Achievable_Number/Solution.java
@@ -2,6 +2,6 @@ package LeetCode.task_2769_Find_the_Maximum_Achievable_Number;
 
 public class Solution {
     public int theMaximumAchievableX(int num, int t) {
-        return num+(2*t);
+        return num + (2 * t);
     }
 }
