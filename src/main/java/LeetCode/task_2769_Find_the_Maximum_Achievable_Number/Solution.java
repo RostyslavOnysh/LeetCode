3b037f0d6750c5511a7883ca4b8f7b77ada5757b
Ros <traine41@icloud.com>
2024-08-10 18:22:14 +0100

    LeetCode.task_2769_Find_the_Maximum_Achievable_Number;

---
+++ src/main/java/LeetCode/task_2769_Find_the_Maximum_Achievable_Number/Solution.java
@@ -0,0 +1,7 @@
+package LeetCode.task_2769_Find_the_Maximum_Achievable_Number;
+
+public class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num+(2*t);
+    }
+}
