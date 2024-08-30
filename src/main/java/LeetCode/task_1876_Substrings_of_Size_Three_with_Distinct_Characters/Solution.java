fc91da1e4f232b2f5fa8a795fb918c73f243abe3
Ros <traine41@icloud.com>
2024-08-30 15:51:48 +0100

    task_1876_Substrings_of_Size_Three_with_Distinct_Characters;

--- src/main/java/LeetCode/task_1876_Substrings_of_Size_Three_with_Distinct_Characters/Solution.java
+++ src/main/java/LeetCode/task_1876_Substrings_of_Size_Three_with_Distinct_Characters/Solution.java
@@ -2,18 +2,16 @@ package LeetCode.task_1876_Substrings_of_Size_Three_with_Distinct_Characters;
 
 public class Solution {
     public int countGoodSubstrings(String s) {
-        int res = 0, left = 0;
-        for (int rigth = 0; rigth < s.length(); rigth++) {
-            if (rigth - left + 1 == 3) {
-                if (s.charAt(left) != s.charAt(left + 1) &&
-                        s.charAt(left + 1) != s.charAt(left + 2) &&
-                        s.charAt(left) != s.charAt(left + 2)) {
-                    res++;
-                }
-                left++;
-            }
+        int res = 0;
 
+        for (int right = 2; right < s.length(); right++) {
+            if (s.charAt(right - 2) != s.charAt(right - 1) &&
+                    s.charAt(right - 1) != s.charAt(right) &&
+                    s.charAt(right - 2) != s.charAt(right)) {
+                res++;
+            }
         }
+
         return res;
     }
 }
