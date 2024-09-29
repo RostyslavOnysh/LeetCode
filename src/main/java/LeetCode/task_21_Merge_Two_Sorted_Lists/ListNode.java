ea5fad0711effc36384358654265c33fdd25cb1c
Ros <traine41@icloud.com>
2024-09-29 12:52:45 +0100

    task_21_Merge_Two_Sorted_Lists;

---
+++ src/main/java/LeetCode/task_21_Merge_Two_Sorted_Lists/ListNode.java
@@ -0,0 +1,19 @@
+package LeetCode.task_21_Merge_Two_Sorted_Lists;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
