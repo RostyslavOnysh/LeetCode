0673eb49c89440452ee41db9f61286ce7ad015a2
Ros <traine41@icloud.com>
2024-11-19 15:49:32 +0000

    task_2747_Count_Zero_Request_Servers;

---
+++ src/main/java/LeetCode/task_2747_Count_Zero_Request_Servers/Solution.java
@@ -0,0 +1,34 @@
+package LeetCode.task_2747_Count_Zero_Request_Servers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+    public int[] countServers(int n, int[][] logs, int x, int[] queries) {
+        int m = queries.length;
+        int[] counts = new int[n + 1], output = new int[m];
+        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
+        Arrays.sort(logs, Comparator.comparingInt(log -> log[1]));
+        for (int i = 0; i < m; i++) {
+            map.putIfAbsent(queries[i], new ArrayList<>());
+            map.get(queries[i]).add(i);
+        }
+        int a = 0, b = 0, c = n;
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : map.entrySet()) {
+            int t = entry.getKey();
+            while (a < logs.length && logs[a][1] <= t) {
+                int s = logs[a++][0];
+                if (counts[s]++ == 0) c--;
+            }
+            while (b < a && logs[b][1] < t - x) {
+                int s = logs[b++][0];
+                if (--counts[s] == 0) c++;
+            }
+            for (int i : entry.getValue()) output[i] = c;
+        }
+        return output;
+    }
+}
