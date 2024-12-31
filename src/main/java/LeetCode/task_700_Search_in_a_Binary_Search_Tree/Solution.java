6e3d99b1e61f0c30e7b562d5a0eb205ff489eae3
Ros <traine41@icloud.com>
2024-12-31 09:49:39 +0000

    LeetCode.task_700_Search_in_a_Binary_Search_Tree;

---
+++ src/main/java/LeetCode/task_700_Search_in_a_Binary_Search_Tree/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode.task_700_Search_in_a_Binary_Search_Tree;
+
+public class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
